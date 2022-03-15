38083e937bd92292ca53bea9ea8b9f0db8b04faa
Dawid Studziski <dawidstu@gmail.com>
2022-03-15 21:20:48 +0100

    Making a player & background

---
+++ Kurczaki/Assets/Scripts/Start_Script.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Start_Script : MonoBehaviour
+{
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene(1);
+
+
+    }
+
+    public void closeGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
