8f4f6b708345548afb2d018d818575621935ac58
Dawid Studziski <dawidstu@gmail.com>
2022-03-31 23:11:31 +0200

    Obrażenia po kolizji

--- Kurczaki/Assets/Scripts/DamageByColision.cs
+++ Kurczaki/Assets/Scripts/DamageByColision.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class DamageByColision : MonoBehaviour
 {
-    public float damage = 1;
+    //public float damage = 1;
+    public int health = 1;
+    public float invulnTime = 0f;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    int correctLayer;
+
+    private void Start()
     {
-        
+        correctLayer = gameObject.layer;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (invulnTime <= 0)
+        {
+            health--;
+            invulnTime = 2f;
+
+            gameObject.layer = 8;
+        }
+    }
+
+    void Update()
+    {
+        invulnTime -= Time.deltaTime;
+
+
+        if(invulnTime <= 0)
+        {
+            gameObject.layer = correctLayer;
+        }
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 
 }
